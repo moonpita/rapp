5726cae4d24540d562cec2b7557c2e03fbe6300d
Igor Malyugin <i.malyugin00@gmail.com>
2023-08-13 12:37:31 +0300

    added classnames, styles, themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+import { classNames } from './helpers/classNames/classNames';
 
-import './index.scss';
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { Suspense } from "react";
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
 
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <Link to="/">Main</Link>
-        <Link to="/about">About</Link>
-        <Suspense fallback={<div>loading...</div>}>
-          <Routes>
-            <Route path={'/'} element={<MainPageAsync />} />
-            <Route path={'/about'} element={<AboutPageAsync />} />
-          </Routes>
-        </Suspense>
-      </div>
-    </BrowserRouter>
-  )
-}
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
-import { render } from "react-dom";
-import { App } from "./App";
+import {render} from "react-dom";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
-  <App />,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
