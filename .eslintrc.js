c4b4a305969f35ca6fedda430b476d0c327f6875
Igor Malyugin <i.malyugin00@gmail.com>
2023-08-20 18:26:11 +0300

    change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { ignoreComments: true, code: 120 }],
   },
   globals: {
     __IS_DEV__: true,
