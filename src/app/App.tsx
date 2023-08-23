f109937bcd8d47c4707b011d743ce8ef7572a19f
Igor Malyugin <i.malyugin00@gmail.com>
2023-08-23 15:58:01 +0300

    notFoundPage, ErrorBOundary, PageLoader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 
--- src/app/providers/routes/ui/AppRouter.tsx
+++ src/app/providers/routes/ui/AppRouter.tsx
@@ -1,11 +1,10 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 
 const AppRouter = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {Object.values(routeConfig).map(({ element, path }) => (
         <Route
